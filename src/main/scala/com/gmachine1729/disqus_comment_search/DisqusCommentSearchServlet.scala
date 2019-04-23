1fb928fe42ef0cc9bae770b3450376bfc4c7cae4
gmachine1729 <gmachine1729@foxmail.com>
2019-04-23 22:59:23 +0800

    change printlns to logger.log, add copyright
    downloaded 4281 of BobSykes's comments in 295.337 seconds

--- src/main/scala/com/gmachine1729/disqus_comment_search/DisqusCommentSearchServlet.scala
+++ src/main/scala/com/gmachine1729/disqus_comment_search/DisqusCommentSearchServlet.scala
@@ -4,7 +4,6 @@ import java.net.URL
 import java.util.Properties
 
 import com.gmachine1729.disqus_comment_search.db.DatabaseSessionSupport
-import com.gmachine1729.disqus_comment_search.VisitLogger
 import com.gmachine1729.disqus_comment_search.models.{Tables, Visit}
 import org.scalatra._
 import org.scalatra.scalate.ScalateSupport
@@ -14,6 +13,7 @@ import org.jsoup.Jsoup
 import edu.stanford.nlp.pipeline._
 import edu.stanford.nlp.ling.CoreAnnotations._
 import org.scalatra.i18n.I18nSupport
+import org.slf4j.LoggerFactory
 import scalaj.http.Http
 
 import scala.collection.JavaConversions._
@@ -22,18 +22,26 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.sys.process._
-import scala.util.{Failure, Success, Try}
+import scala.util.{Success, Try}
 
 
 class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport with FormSupport with I18nSupport with SessionSupport
   with DatabaseSessionSupport {
+  val logger = LoggerFactory.getLogger(getClass)
   val API_KEY = "E8Uh5l5fHZ6gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F"
   val BASE_URL = "https://disqus.com/api/3.0/timelines/activities"
-  val MIN_LIMIT: Integer = 5
+  val MIN_LIMIT: Integer = 10
   val MAX_LIMIT: Integer = 100
   val props = new Properties()
   props.put("annotators", "tokenize, ssplit, pos, lemma")
   val pipeline = new StanfordCoreNLP(props)
+
+  def time[R](logMsg: String)(block: => R): R= {
+    val s = System.currentTimeMillis
+    val res = block
+    logger.info(String.format("%s in %s ms", logMsg, (System.currentTimeMillis - s).toString))
+    res
+  }
 
   def getUrl(username: String, cursor: String, limit: Integer): String = {
     String.format("%s?type=profile&index=comments&target=user:username:%s&cursor=%s&limit=%d&api_key=%s",
@@ -62,52 +70,38 @@ class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport wit
   }
 
   def downloadComments(username: String, searchTermsLemmatized: Set[String], limitParam: Int, commentDownloadLimit: Int, matchAllTerms: Boolean)(
-      cursor: String, numDownloaded: Int, accumComments: ArrayBuffer[Comment], prevCommentBatch: Option[Future[Iterable[Comment]]]): String = {
-    val t0 = System.nanoTime()
-    val filteredJson = (new URL(getUrl(username, cursor, limitParam)) #> String.format("python src/main/python/filter_json_response.py %s", username)).!!
-    val t1 = System.nanoTime()
-    val secondsTaken = (t1-t0) / 1000000000.0
-    println("Url: " + getUrl(username, cursor, limitParam))
-    println("Retrieving and filtering json took: " + secondsTaken)
+      cursor: String, numDownloaded: Int, accumComments: ArrayBuffer[Comment], prevCommentBatch: Option[Future[Seq[Comment]]]): String = {
+    val url = getUrl(username, cursor, limitParam)
+    val filteredJson = time(String.format("Retrieved from %s", url)) {
+      (new URL(url) #> String.format("python src/main/python/filter_json_response.py %s", username)).!!
+    }
     prevCommentBatch match {
       case Some(future) => {
-        val t0 = System.nanoTime()
-        val filteredCommentBatch = Await.result[Iterable[Comment]](future, Duration.Inf)
-        val t1 = System.nanoTime()
-        val secondsTaken = (t1-t0) / 1000000000.0
-        println("Waited time for filter future: " + secondsTaken)
+        val filteredCommentBatch = time("Waited to search through comments") {
+          Await.result[Seq[Comment]](future, Duration.Inf)
+        }
         accumComments.appendAll(filteredCommentBatch)
       }
       case None =>
     }
     val commentsResponse = read[CommentsResponse](filteredJson)
+    logger.info(String.format("Deserialized %d comments.", new Integer(commentsResponse.response.objects.size)))
     val commentBatch = Future {
-        commentsResponse.response.objects.map(_._2).filter(comment => containsSearchTerms(searchTermsLemmatized, matchAllTerms)(comment) > 0)
+        commentsResponse.response.objects.map(_._2).filter(comment => containsSearchTerms(searchTermsLemmatized, matchAllTerms)(comment) > 0).toSeq
     }
 
     def commentsHtml: String = {
-      val t2 = System.nanoTime()
-      val filteredCommentBatch = Await.result[Iterable[Comment]](commentBatch, Duration.Inf)
-      val t3 = System.nanoTime()
-      val secondsTaken2 = (t3-t2) / 1000000000.0
-      System.out.println("Waiting for final comment batch took: " + secondsTaken2)
+      val filteredCommentBatch = time("Searched through comments"){
+        Await.result[Seq[Comment]](commentBatch, Duration.Inf)
+      }
       accumComments.appendAll(filteredCommentBatch)
-      val t0 = System.nanoTime()
-      //val htmlResponse = accumComments.sorted.map(_.toHtml).foldLeft("")(_ ++ "\n" ++ _)
       val htmlResponse = new StringBuffer(String.format("<h4><b>%d</b> results</h4><hr>", new Integer(accumComments.length)))
-      println("ArrayBuffer[Comment] length: " + accumComments.length)
       for (comment <- accumComments.sorted) {
         htmlResponse.append(comment.toHtml)
         htmlResponse.append("<hr>")
       }
-      val t1 = System.nanoTime()
-      val secondsTaken = (t1-t0) / 1000000000.0
-      System.out.println(secondsTaken)
-      println("ArrayBuffer[Comment] length again: " + accumComments.length)
       htmlResponse.toString
     }
-
-    System.out.println(commentsResponse.response.objects.size)
 
     commentsResponse.cursor.next match {
       case null       => commentsHtml
@@ -142,6 +136,7 @@ class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport wit
   }
 
   post("/feedback") {
+    contentType = "text/plain"
     val form = mapping(
       "name" -> label("Name", text(required, maxlength(100))),
       "email" -> label("Email", optional(text(pattern(".+@.+"), maxlength(40)))),
@@ -162,13 +157,14 @@ class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport wit
     }
 
     def formErrorToHtmlResponse(hasErrors: Seq[(String, String)]): String = {
-      val errorMsg = hasErrors.map(_._2).fold("")(_ ++ "<br>" ++ _)
+      val errorMsg = hasErrors.map(_._2).fold("")(_ ++ "\n" ++ _)
       errorMsg
     }
-    ssp("/WEB-INF/templates/views/feedback.ssp", "feedbackSubmissionResponse" -> validate(form)(formErrorToHtmlResponse, submitFeedback))
+    validate(form)(formErrorToHtmlResponse, submitFeedback)
   }
 
   get("/search") {
+    val startTime = System.currentTimeMillis()
     val visitFuture: Future[Visit] = VisitLogger.genVisitRecord(request, VisitLogger.VisitType.SEARCH.id)
 
     def formToHtmlResponse(validatedForm: ValidationForm): (ValidationForm, String) = {
@@ -181,27 +177,24 @@ class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport wit
           val visit = Await.result(visitFuture, Duration.Inf)
           visit.setFormParameters(validatedForm)
           Visit.create(visit)
-          println("logged no username visit to db")
+          logger.info("wrote no username found visit to db")
         }
         (validatedForm, String.format ("<div>Did not find any Disqus user with username <b>%s</b></div>", username))
       } else {
         val cursor: String = ""
         val accumComments: ArrayBuffer[Comment] = new ArrayBuffer[Comment] (100)
-        val t2 = System.nanoTime ()
-        val res = downloadComments (username, getQueryTokens (query), getDisqusLimitParam (commentDownloadLimit), commentDownloadLimit, matchAllTerms) (
+        val searchResults = downloadComments (username, getQueryTokens (query), getDisqusLimitParam (commentDownloadLimit), commentDownloadLimit, matchAllTerms) (
           cursor, 0, accumComments, None)
-        val t3 = System.nanoTime ()
-        val secondsTaken2 = (t3 - t2) / 1000000000.0
-        System.out.println ("total time: " + secondsTaken2)
         Future {
           val visit = Await.result(visitFuture, Duration.Inf)
           visit.setFormParameters(validatedForm)
           visit.foundUser = true
           visit.numResults = Some(accumComments.length)
+          visit.latency = Some((System.currentTimeMillis() - startTime)/1000.0)
           Visit.create(visit)
-          println("logged returned results visit to db")
+          logger.info("wrote returned search results visit to db")
         }
-        (validatedForm, res)
+        (validatedForm, searchResults)
       }
     }
 
@@ -209,7 +202,7 @@ class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport wit
       Future {
         val visit = Await.result(visitFuture, Duration.Inf)
         Visit.create(visit)
-        println("logged form validation error visit to db")
+        logger.info("wrote form validation error visit to db")
       }
       val limit = Try(params("comment_download_limit").toInt) match {
         case Success(value) => value
@@ -235,9 +228,8 @@ class DisqusCommentSearchServlet extends ScalatraServlet with ScalateSupport wit
     contentType = "text/html"
     val visitFuture: Future[Visit] = VisitLogger.genVisitRecord(request, VisitLogger.VisitType.HOME_PAGE.id)
     val _ = Future {
-      val visit = Await.result(visitFuture, Duration.Inf)
       Visit.create(Await.result(visitFuture, Duration.Inf))
-      println("logged visit to db")
+      logger.info("wrote home page visit to db")
     }
     ssp("/WEB-INF/templates/views/results.ssp", "form" -> ValidationForm("", Some(""), 100, false), "htmlResponse" -> "")
   }
