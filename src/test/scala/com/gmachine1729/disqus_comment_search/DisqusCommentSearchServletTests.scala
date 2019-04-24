35f963c32bfe6d1cc55e463cf6b707ec855e3e6c
gmachine1729 <gmachine1729@foxmail.com>
2019-04-24 20:20:04 +0800

    generate and add sql file for visit table

--- src/test/scala/com/gmachine1729/disqus_comment_search/DisqusCommentSearchServletTests.scala
+++ src/test/scala/com/gmachine1729/disqus_comment_search/DisqusCommentSearchServletTests.scala
@@ -1,11 +1,12 @@
 package com.gmachine1729.disqus_comment_search
 
 import com.gmachine1729.disqus_comment_search.db.DatabaseInit
-import com.gmachine1729.disqus_comment_search.models.Visit
+import com.gmachine1729.disqus_comment_search.models.{Tables, Visit}
 import org.junit.runner.RunWith
 import org.scalatest.BeforeAndAfter
 import org.scalatest.junit.JUnitRunner
 import org.scalatra.test.scalatest._
+import org.squeryl.PrimitiveTypeMode.inTransaction
 
 @RunWith(classOf[JUnitRunner])
 class DisqusCommentSearchServletTests extends ScalatraFunSuite with DatabaseInit with BeforeAndAfter {
@@ -23,8 +24,10 @@ class DisqusCommentSearchServletTests extends ScalatraFunSuite with DatabaseInit
   test("GET / on DisqusCommentSearchServlet should return status 200") {
     get("/") {
       /*val visit = new Visit()
-      val res = Visit.create(visit)
-      println(res)*/
+      val res = Visit.create(visit)*/
+      inTransaction {
+        Tables.printDdl
+      }
       status should equal (200)
     }
   }
